99341c7aa64f7abdfda1388c36d2f3a76683047d
Guillaume Lacasse <glacasse@gmail.com>
2021-03-02 20:19:10 -0500

    work inprogress

--- MusicChordIntervalEarTraining/MusicPlayer/MusicPlayer.cs
+++ MusicChordIntervalEarTraining/MusicPlayer/MusicPlayer.cs
@@ -14,24 +14,33 @@ namespace MusicChordIntervalEarTraining
 
         private bool isPlaying = false;
 
+        private bool isFinishedPlaying = true;
+
         private Random random;
 
-        public MusicPlayer(Random random)
+        private int midiChannel;
+
+        private OutputDevice outputDevice;
+
+        public MusicPlayer(Random random, OutputDevice outputDevice, int midiChannel)
         {
             this.random = random;
+            this.midiChannel = midiChannel;
+            this.outputDevice = outputDevice;
         }
 
         public void Play(Progression progression)
         {
             this.Stop();
 
+            lock (this.playerLock)
+            {
+                this.isPlaying = true;
+                this.isFinishedPlaying = false;
+            }
+
             Thread playerThread = new Thread(() =>
             {
-                lock (this.playerLock)
-                {
-                    this.isPlaying = true;
-                }
-
                 while (isPlaying)
                 {
                     foreach (Chord chord in progression)
@@ -46,6 +55,11 @@ namespace MusicChordIntervalEarTraining
 
                     this.Play(progression.Last());
                 }
+
+                lock (this.playerLock)
+                {
+                    this.isFinishedPlaying = true;
+                }
             });
             playerThread.IsBackground = true;
             playerThread.Start();
@@ -53,29 +67,61 @@ namespace MusicChordIntervalEarTraining
 
         private void Play(Chord chord)
         {
-            foreach (int note in chord)
+            int noteCount = 0;
+            int[] notes = this.GetNotes(chord);
+            while (true)
             {
-                if (random.Next(0, 2) == 0)
+                Shuffle(notes);
+                foreach (int note in notes)
                 {
-                    Play(64 + note);
-                }
-                else
-                {
-                    Play(64 + note + 12);
-                }
-                
-                if (!isPlaying)
-                {
-                    break;
+                    if (random.Next(0, 2) == 0)
+                    {
+                        Play(64 + note);
+                    }
+                    else
+                    {
+                        Play(64 + note + 12);
+                    }
+
+                    if (!isPlaying)
+                    {
+                        return;
+                    }
+                    ++noteCount;
+
+                    if (noteCount >= 16)
+                    {
+                        return;
+                    }
                 }
             }
         }
 
-        private void Play(int pitch)
+        private int[] GetNotes(Chord chord)
         {
-            ChannelMessage noteOn = this.BuildChannelMessage(pitch, true);
+            #warning Implement
+            return new int[] { 1, 2, 3 };
+        }
+
+        private void Shuffle(int[] notes)
+        {
+            #warning Implement
+        }
+
+        private void Play(int pitch)
+        {            
+            ChannelMessage noteOn = this.BuildChannelMessage(pitch, ChannelCommand.NoteOn);
+            this.outputDevice.Send(noteOn);
             Thread.Sleep(100);
-            ChannelMessage noteOn = this.BuildChannelMessage(pitch, true);
+            ChannelMessage noteOff = this.BuildChannelMessage(pitch, ChannelCommand.NoteOff);
+            this.outputDevice.Send(noteOff);
+            Thread.Sleep(10);
+        }
+
+        private ChannelMessage BuildChannelMessage(int pitch, ChannelCommand command)
+        {
+            ChannelMessage channelMessage = new ChannelMessage(command, midiChannel, pitch, 127);
+            return channelMessage;
         }
 
         public void Stop()
@@ -83,6 +129,11 @@ namespace MusicChordIntervalEarTraining
             lock (this.playerLock)
             {
                 this.isPlaying = false;
+            }
+
+            while (!isFinishedPlaying)
+            {
+                Thread.Sleep(10);
             }
         }
     }
--- MusicChordIntervalEarTraining/Program.cs
+++ MusicChordIntervalEarTraining/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sanford.Multimedia.Midi;
+using System;
 
 namespace MusicChordIntervalEarTraining
 {
@@ -6,49 +7,57 @@ namespace MusicChordIntervalEarTraining
     {
         static void Main(string[] args)
         {
-            QuestionBuilder questionBuilder = new QuestionBuilder(new Random(), 2, false, false);
+            const int midiChannel = 1;
+            Random random = new Random();
+            QuestionBuilder questionBuilder = new QuestionBuilder(random, 2, false, false);
             InputParser inputParser = new InputParser(new IntervalParser(), new ChordTypeParser());
-            MusicPlayer musicPlayer = new MusicPlayer();
-
-            while (true)
+            using (OutputDevice outputDevice = new OutputDevice(0))
             {
-                Progression progression = questionBuilder.BuildProgression();
+                //outputDevice.Send(new ChannelMessage(ChannelCommand.NoteOn, 1, 64, 64));
 
-                Console.WriteLine(progression);
+                MusicPlayer musicPlayer = new MusicPlayer(random, outputDevice, midiChannel);
 
-                Console.WriteLine("Intervals: 0, 2b, 2, 3m, 3M, 4, 4A, 5, 6b, 6, 7m, 7M");
-                Console.WriteLine("Chord types: M, m, M7, m7, 7, dim7, aug, hdim, dim");
-                Console.WriteLine("Type something like M 4 m (for minor plagal cadence), exit to quit");
-
-                musicPlayer.Play(progression);
-
-                string input = Console.ReadLine();
-
-                musicPlayer.Stop();
-
-                if (input.ToUpperInvariant().Contains("EXIT"))
+                while (true)
                 {
-                    break;
+                    Progression progression = questionBuilder.BuildProgression();
+
+                    Console.WriteLine(progression);
+
+                    Console.WriteLine("Intervals: 0, 2b, 2, 3m, 3M, 4, 4A, 5, 6b, 6, 7m, 7M");
+                    Console.WriteLine("Chord types: M, m, M7, m7, 7, dim7, aug, hdim, dim");
+                    Console.WriteLine("Type something like M 4 m (for minor plagal cadence), exit to quit");
+
+                    musicPlayer.Play(progression);
+
+                    string input = Console.ReadLine();
+
+                    musicPlayer.Stop();
+
+                    if (input.ToUpperInvariant().Contains("EXIT"))
+                    {
+                        break;
+                    }
+
+                    Progression parsedInput = inputParser.Parse(input, progression.PitchOffset);
+
+                    Console.WriteLine("Parsed as " + parsedInput.ToString());
+
+                    if (progression.ToString() == parsedInput.ToString())
+                    {
+                        Console.WriteLine("MATCH!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("WRONG, it is " + progression.ToString());
+                    }
+
+
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
                 }
 
-                Progression parsedInput = inputParser.Parse(input, progression.PitchOffset);
-
-                Console.WriteLine("Parsed as " + parsedInput.ToString());
-
-                if (progression.ToString() == parsedInput.ToString())
-                {
-                    Console.WriteLine("MATCH!");
-                }
-                else
-                {
-                    Console.WriteLine("WRONG, it is " + progression.ToString());
-                }
-
-
-                Console.WriteLine("");
-                Console.WriteLine("");
-                Console.WriteLine("");
-            };
+            }
         }
     }
 }
